c7be1bcf11c8085af99af1f54e67a0c2e412e2ba
小白_2312 <17769013320@163.com>
2015-12-12 16:46:22 +0800

    NumberOne的第一次commit

---
+++ app/src/main/java/com/test/numberone/MainActivity.java
@@ -0,0 +1,19 @@
+package com.test.numberone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*
+    https://git.oschina.net/1031066280/ThroughWaring.git
+    https://git.oschina.net/tudounigemalingshu/Test.git
+    https://git.oschina.net/tudounigemalingshu/MyFirst.git
+     */
+}
